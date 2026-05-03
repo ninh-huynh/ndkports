b2a6d59c2e991ee6f25b654c41c36104acaa5800
Ninh Huynh <kimninh1610@gmail.com>
2026-05-03 22:29:08 +0700

    allow prefabGenerated property is optional since not all PortTask require it (RustJniPortTask)

--- plugin/src/main/kotlin/com/android/ndkports/PortTask.kt
+++ plugin/src/main/kotlin/com/android/ndkports/PortTask.kt
@@ -9,6 +9,7 @@ import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -25,6 +26,7 @@ abstract class PortTask : DefaultTask() {
     @get:OutputDirectory
     abstract val installDir: DirectoryProperty
 
+    @get:Optional
     @get:InputDirectory
     abstract val prefabGenerated: DirectoryProperty
 
